67ea3d58be4d1afe6fa0449783029353486276c2
lhl1 <56253099+lhl1@users.noreply.github.com>
2021-01-16 21:53:39 +0800

    Update Gitee与GitHub网站上传的切换.cpp

--- Gitee与GitHub网站上传的切换.cpp
+++ Gitee与GitHub网站上传的切换.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
- 
+//LHL
 int main(int argc, char *argv[])
 {
 	std::string oldName, hName,eName,tName;
 #ifdef _WIN32
 	oldName = "C:\\Temp\\blog\\_config.yml";
-	eName = "C:\\Temp\\blog\\_confige.yml";
+	eName = "C:\\Temp\\blog\\_confige.yml";//LHL
 	hName = "C:\\Temp\\blog\\_configh.yml";
 	tName= "C:\\Temp\\blog\\_configt.yml";
 #endif
  
-	if (!rename(eName.c_str(), tName.c_str()))
+	if (!rename(eName.c_str(), tName.c_str()))//LHL
 	{
 		std::cout << "    准备更改为Gitee模式"<< std::endl;
 		if (!rename(oldName.c_str(), hName.c_str()))
 			std::cout << "    副本更改为Github模式"<< std::endl;
-		else std::cout << "    副本更改为Github模式失败！"<< std::endl;
+		else std::cout << "    副本更改为Github模式失败！"<< std::endl;//LHL
 		
 		if (!rename(tName.c_str(), oldName.c_str()))
-		std::cout << "    更改为Gitee模式成功！"<< std::endl;
-		else std::cout << "    rename error "<< std::endl;
+		std::cout << "    更改为Gitee模式成功！"<< std::endl;//LHL
+		else std::cout << "    rename error "<< std::endl;//LHL
 	}
 	else
-	{
+	{//LHL
 		std::cout << "    rename error或更改为Github模式 "<< std::endl;
 		if (!rename(hName.c_str(), tName.c_str()))
 		std::cout << "    准备更改为Github模式"<< std::endl;
@@ -33,10 +33,10 @@ int main(int argc, char *argv[])
 			std::cout << "    副本更改为Gitee模式"<< std::endl;
 		else std::cout << "    副本更改为Gitee模式失败！"<< std::endl;
 		
-		if (!rename(tName.c_str(), oldName.c_str()))
+		if (!rename(tName.c_str(), oldName.c_str()))//LHL
 		std::cout << "    更改为Github模式成功！"<< std::endl;
 		else std::cout << "rename error "<< std::endl;
 	}
- 	system("pause");
+ 	system("pause");//LHL
 	return 0;
 }
